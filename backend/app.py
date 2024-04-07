62ab488c4a5eae1a950ca08ba23a06f83671db11
Kuzuma-kun <ngoccatt@gmail.com>
2024-04-07 19:02:07 +0700

    Analysis_Cat

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,11 @@ from flask import Flask
 from connector import cnt
 from flink import flink_tables
 
-env_setting = EnvironmentSettings.in_streaming_mode()
+env_setting = EnvironmentSettings.in_batch_mode()
 table_env = TableEnvironment.create(env_setting)
 
 flink_tables.createTable(table_env, cnt)
+flink_tables.getAnalitics(table_env, cnt)
 
 app = Flask(__name__)
 
@@ -23,6 +24,102 @@ def formatResult(status, records):
     data = df.values.tolist()
     return {"status": status, "data": data}
 
+# doanh thu theo tung quoc gia
+@app.route("/revenue/countries")
+def revenue_report():
+    result = "Empty"
+    df = flink_tables.pdf_revenue
+    df = df.groupby("country", as_index=False)[["revenue"]].sum()
+    df = df.sort_values(by=("revenue"), ascending=False)
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
+
+#doanh thu theo tung thang
+@app.route("/revenue/monthly")
+def revenue_monthly():
+    result = "Empty"
+    df = flink_tables.pdf_revenue
+    df["time"] = df["year"].astype("str") + '-' + df["month"].astype("str")
+    df = df.groupby("time", as_index=False, sort=False)[["revenue"]].sum()
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
+
+#doanh thu theo tung nam
+@app.route("/revenue/yearly")
+def revenue_yearly():
+    result = "Empty"
+    df = flink_tables.pdf_revenue
+    df = df.groupby("year", as_index=False, sort=False)[["revenue"]].sum()
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
+
+#so don hang theo tung quoc gia
+@app.route("/orders/countries")
+def orders_coun():
+    result = "Empty"
+    df = flink_tables.pdf_avg_revenue
+    df = df.groupby("country", as_index=False, sort=False)[["num_orders"]].sum()
+    df = df.sort_values(by=["num_orders"], ascending = False)
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
+
+#trung binh gia tri don hang theo tung quoc gia
+@app.route("/orders/avg")
+def orders_avg():
+    result = "Empty"
+    df = flink_tables.pdf_avg_revenue
+    df = df[
+        ["country", "num_orders", "total_revenue"]
+        ].groupby("country", as_index=False, sort=False)[["num_orders", "total_revenue"]].sum()
+    df["avg"] = df["total_revenue"] / df["num_orders"]
+    df = df.sort_values(by=["avg"], ascending = False)
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
+
+
+#so don hang duoc dat theo khung gio
+@app.route("/orders/timeslot")
+def orders_timeslot():
+    result = "Empty"
+    df = flink_tables.pdf_order_timeslot
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
+
+#tong so don hang duoc dat theo khung gio (xuyen quoc gia)
+@app.route("/orders/timeslot_sum")
+def orders_timeslot_sum():
+    result = "Empty"
+    df = flink_tables.pdf_order_timeslot
+    df = df.iloc[:, 1:]
+    df = df.agg(["sum"], axis = 0)
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
+
+#tong so luong don hang bi huy tren tong don hang o tung quoc gia
+@app.route("/orders/cancelled")
+def orders_cancelled():
+    result = "Empty"
+    df = flink_tables.pdf_cancel_rate
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
+
+#tong so luong don hang bi huy tren tong don hang
+@app.route("/orders/cancelled_sum")
+def orders_cancelled_sum():
+    result = "Empty"
+    df = flink_tables.pdf_cancel_rate
+    df = df.iloc[:, 1:].agg(["sum"], axis = 0)
+    print(df)
+    result = {"status": True, "header": df.columns.tolist(), "data": df.values.tolist()}
+    return result
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000 ,debug=True)
--- backend/flink.py
+++ backend/flink.py
@@ -39,4 +39,94 @@ class Flink:
         except Exception as e:
             print(e)
             
+    def getAnalitics(self, table_env, cnt):
+        try: 
+            self.pdf_revenue = cnt.fetchData(
+                                        """
+                    select country, (date_part(year, invoiceDate)) as year, 
+                        (date_part(month, invoiceDate)) as month, 
+                        sum(unitprice * quantity) as revenue
+                    from orders, belong, product, customer
+                    where orders.cancelled = FALSE
+                    and belong.orderid = orders.id
+                    and belong.productid = product.id
+                    and orders.customerid = customer.id
+                    group by country, year, month
+                    order by 1, 2, 3;
+                                        """
+                                        )
+            
+            self.pdf_avg_revenue = cnt.fetchData(
+                                        """
+                    SELECT 
+                        country, 
+                        DATE_PART('year', invoiceDate) AS year, 
+                        DATE_PART('month', invoiceDate) AS month, 
+                        COUNT(DISTINCT orders.id) AS num_orders,
+                        SUM(unitprice * quantity) AS total_revenue,
+                        SUM(unitprice * quantity) / COUNT(DISTINCT orders.id) AS avg_price_per_order
+                    FROM 
+                        orders
+                    JOIN 
+                        belong ON belong.orderid = orders.id
+                    JOIN 
+                        product ON belong.productid = product.id
+                    JOIN 
+                        customer ON orders.customerid = customer.id
+                    WHERE 
+                        orders.cancelled = FALSE
+                    GROUP BY 
+                        country, year, month
+                    ORDER BY 
+                        country, year, month, num_orders;
+                                        """
+                                        )
+            
+            self.pdf_order_timeslot = cnt.fetchData(
+                                        """
+                    SELECT 
+                        country,
+                        COUNT(DISTINCT CASE WHEN EXTRACT('hour' FROM invoiceTime) BETWEEN 0 AND 2 THEN orders.id END) AS h0_3,
+                        COUNT(DISTINCT CASE WHEN EXTRACT('hour' FROM invoiceTime) BETWEEN 3 AND 5 THEN orders.id END) AS h3_6,
+                        COUNT(DISTINCT CASE WHEN EXTRACT('hour' FROM invoiceTime) BETWEEN 6 AND 8 THEN orders.id END) AS h6_9,
+                        COUNT(DISTINCT CASE WHEN EXTRACT('hour' FROM invoiceTime) BETWEEN 9 AND 11 THEN orders.id END) AS h9_12,
+                        COUNT(DISTINCT CASE WHEN EXTRACT('hour' FROM invoiceTime) BETWEEN 12 AND 14 THEN orders.id END) AS h12_15,
+                        COUNT(DISTINCT CASE WHEN EXTRACT('hour' FROM invoiceTime) BETWEEN 15 AND 17 THEN orders.id END) AS h15_18,
+                        COUNT(DISTINCT CASE WHEN EXTRACT('hour' FROM invoiceTime) BETWEEN 18 AND 20 THEN orders.id END) AS h18_21,
+                        COUNT(DISTINCT CASE WHEN EXTRACT('hour' FROM invoiceTime) BETWEEN 21 AND 23 THEN orders.id END) AS h21_24
+                    FROM 
+                        orders
+                    JOIN 
+                        customer ON orders.customerid = customer.id
+                    WHERE 
+                        orders.cancelled = FALSE
+                    GROUP BY 
+                        country
+                    ORDER BY 
+                        h12_15 DESC, country;
+                                        """
+                                        )
+            
+            self.pdf_cancel_rate = cnt.fetchData(
+                                        """
+                    SELECT 
+                        country,
+                        COUNT(DISTINCT CASE cancelled WHEN TRUE THEN orders.id END) AS cancel_orders,
+                        COUNT(DISTINCT orders.id) as total_orders
+                    FROM 
+                        orders
+                    JOIN 
+                        customer ON orders.customerid = customer.id
+                    GROUP BY 
+                        country
+                    ORDER BY 
+                        total_orders DESC, country ;
+                                        """
+                                        )
+            
+            print("Fetched all table successfully")
+
+        except Exception as e:
+            print(e)
+            
 flink_tables = Flink()
